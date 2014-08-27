d286bb78dd79e20b52ab6abda2da029684ef8632
jhrv <johnny.horvi@gmail.com>
2014-08-27 08:05:23 +0200

    virker uten at det henger lenger, byttet ut form lib

--- app.js
+++ app.js
@@ -7,117 +7,40 @@ var stravaToken = process.env.STRAVA_TOKEN;
 
 util.log("Using Strava token: " + stravaToken);
 
-//var opts = {
-//    host: 'www.strava.com',
-//    path: '/api/v3/uploads',
-//
-//}
-
-//var options = {
-//    uri: "http://www.strava.com/api/v3/uploads",
-//    method: "POST",
-//    headers: {
-//        'Authorization': 'Bearer ' + stravaToken
-//    }
-//};
 var options = {
     headers: {
         'Authorization': 'Bearer ' + stravaToken
     }
 };
-//var callback = function (error, response, body) {
-//    console.log(body);
-//    console.log(error);
-//    console.log(response);
-//};
 
-var upload = function (file) {
-    var r = request.post('http://www.strava.com/api/v3/uploads', options, function optionalCallback(err, httpResponse, body) {
-        if (err) {
-            return console.error('upload failed:', err);
-        }
-        console.log('Upload successful!  Server responded with:', body);
-    })
+var callback = function optionalCallback(err, httpResponse, body) {
+    if (err) {
+        return util.error('Upload failed: ' + err);
+    }
+    util.log('Upload complete! Server responded with: ' + body);
+};
 
-    var form = r.form()
+var uploadActivity = function (file) {
+    var postRequest = request.post('http://www.strava.com/api/v3/uploads', options, callback);
+
+    var form = postRequest.form()
+    form.append('file', fs.createReadStream(file));
     form.append('activity_type', 'ride');
     form.append('data_type', 'fit');
-    form.append('file', fs.createReadStream(file));
 }
 
-upload("/Users/hrv/workspace/supload/johnny.fit");
-upload("/Users/hrv/workspace/supload/testing/balle.fit");
+watch.createMonitor(monitorFolder, function (monitor) {
+    util.log("Started monitoring " + monitorFolder);
 
-//watch.createMonitor(monitorFolder, function (monitor) {
-//    util.log("Started monitoring " + monitorFolder);
-//
-//    monitor.on("created", function (f, stat) {
-//        util.log("Detected new activity file " + f);
-//
-//        var test = request.post(options, callback);
-//
-//        var form = test.form();
-//        var readStream = fs.createReadStream(f);
-//        form.append('file', readStream);
-//        console.log("created read stream");
-//        form.append('activity_type', 'ride');
-//        form.append('data_type','fit');
-//
-//        console.log("done!");
-//        readStream.destroy();
-//
-//    });
-//
-//    monitor.on("removed", function (f, stat) {
-//        util.log(f + " was removed")
-//    });
-//});
+    monitor.on("created", function (f, stat) {
+        util.log("Detected new activity file " + f);
+        uploadActivity(f);
+    });
 
-
-//watch.createMonitor(monitorFolder, function (monitor) {
-//    util.log("Started monitoring " + monitorFolder);
-//
-//    monitor.on("created", function (f, stat) {
-//        util.log("Detected new activity file " + f);
-//        var FormData = require("form-data");
-//        var form = new FormData();
-//        var fs = require("fs");
-//        form.append('file', fs.createReadStream(f));
-//        console.log("created read stream");
-//        form.append('activity_type', 'ride');
-//        form.append('data_type','fit');
-//        form.submit(opts, submitHandler);
-//        console.log("submitted....");
-//    });
-//
-//    monitor.on("removed", function (f, stat) {
-//        util.log(f + " was removed")
-//    });
-//});
-//
-//var submitHandler = function (err, res) {
-//
-//    if (err){
-//        console.log("got err")
-//        throw err;
-//    }
-//
-//    if (res.statusCode === 201) {
-//        util.log("Activity successfully uploaded to Strava");
-//    } else {
-//        util.log("Unable to upload activity to Strava, status code " + res.statusCode);
-//        res.on('data', function (chunk) {
-//            if (chunk){
-//                util.log("chunk:" + chunk);
-//            } else {
-//                util.log("no chunk in data");
-//            }
-//
-//            util.log("Error: " + JSON.parse(chunk).error);
-//        });
-//    }
-//
-//}
+    monitor.on("removed", function (f, stat) {
+        util.log(f + " was removed")
+    });
+});
 
 
 
